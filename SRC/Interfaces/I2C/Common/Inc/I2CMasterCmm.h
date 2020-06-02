2fbaa52e0942490adc967480240b86726e291839
GravitAU <idushy@ya.ru>
2020-06-02 08:51:38 +0400

    так

--- SRC/Interfaces/I2C/Common/Inc/I2CMasterCmm.h
+++ SRC/Interfaces/I2C/Common/Inc/I2CMasterCmm.h
@@ -1,3 +1,60 @@
+#ifndef __MASTERI2C_H
+#define __MASTERI2C_H
+#include "stm32l4xx_hal.h"
+
+
+//фаза1 шаг1. сбор ID с слейвов. запрос мастера
+//фаза1 шаг2. сбор ID с слейвов. ответ слейва
+
+#define	ADDR_BY_MASTER		1	//адрес мастера
+#define	I2CCODE_GET_ID_REQUEST		0xFA	//код - раздача адресов фаза 1 сбор ID
+#define	ST1_SIZE_REQUEST		3	//число байт в запросе "дай ID" устройства ведущего
+#define	SIZE_FACTORY_NUM		4	//длина заводского номера ведомого
+#define	P1S2_SIZE_ANSW		(SIZE_FACTORY_NUM + 1)//ответ - ведомый предьявляет ID
+
+//номера байтов в запросе [Код команды «дай ID» = 0xFA] [Адрес устройства-ведущего] [Зарезервирован]
+#define	P1S1__I_B_CODCMD	0
+#define	P1S1__I_B_ADRMAST 	1
+#define	P1S1__I_B_REZ		2
+
+//прием
+#define	RECEIVE_NEUTRAL			0	//по сути состояние ожидания перехода в рабочий режим
+#define	RECEIVE_START			1	//запуск приема
+#define	RECEIVE_WAIT_DATA		2	//ожидаем запрос
+#define	RECEIVE_YES_ANY_DATA	3	//какието данные приняты
+#define	RECEIVE_TIMOUT			4	//время вышло, данных не было
+
+//предача
+#define	SEND_NEUTRAL		0	//по сути состояние ожидания перехода в рабочий режим
+#define	SEND_START_NOW		1	//запустить передачу
+#define	SEND_WAS_START		2	//запущена передача
+#define	SEND_WAS_GOOD_END	3	//передача успешно завершена
+#define	SEND_TIMOUT			4	//время вышло, данные не переданы
+
+
+
+//сбор ID ведомых этапы
+#define	ST1__DEFVAL				0
+#define	ST1__CMD_GET_ID_SENT		1	//запрос «дай ID» послан
+#define	ST1__ID_GRANTED		2	//пришел ответ на «дай ID»
+
+
+
+#define	SZ_ARR_RX_BUFF		10
+
+#define	SZ_ARR_TX_BUFF		10
+
+
+
+extern void I2CInit(void);
+extern void I2CReceive();
+extern void I2CSend();
+extern void PrepData(void);
+
+#endif /* __MASTERI2C_H */
+
+
+
 #ifndef __I2CSLAVE_H
 #define __I2CSLAVE_H
 #include "stm32f4xx_hal.h"
--- SRC/Interfaces/I2C/Common/Src/I2CMasterCmm.c
+++ SRC/Interfaces/I2C/Common/Src/I2CMasterCmm.c
@@ -1,3 +1,218 @@
+#include "I2CMaster.h"
+#include <stdint.h>
+#include <string.h>
+
+
+
+
+struct I2CUsrData
+{
+	uint8_t	aTxBuffer[SZ_ARR_TX_BUFF];
+	uint16_t sizeTxCmd;
+
+	uint8_t	aRxBuffer[SZ_ARR_RX_BUFF];
+	uint16_t sizeRxCmd;
+
+	uint8_t PhaseSend;
+	uint8_t PhaseReceive;
+	uint8_t PhaseSetAddr;
+};
+
+struct I2CUsrData _usrI2CData;
+
+I2C_HandleTypeDef* _hi2c1;
+
+void SetHederMasterI2C(I2C_HandleTypeDef* hi2c1)
+{
+	_hi2c1 = hi2c1;
+
+}
+
+#define	BTN_PUSH	0
+#define	BTN_RELEASE	1
+
+uint8_t _btnState, _lastBtnState;
+
+void I2CInit()
+{
+		_usrI2CData.PhaseSend = 0;
+		_usrI2CData.PhaseReceive = 0;
+		_usrI2CData.PhaseSetAddr = 0;
+		_usrI2CData.aTxBuffer[0] = I2CCODE_GET_ID_REQUEST;	//тип
+		_usrI2CData.aTxBuffer[1] = ADDR_BY_MASTER;			//адрес
+		_usrI2CData.aTxBuffer[2] = 0xEE;					//адрес
+		memset((uint8_t*)(_usrI2CData.aRxBuffer), (uint16_t) 0, (uint8_t)(SZ_ARR_RX_BUFF));
+
+//		_lastBtnState = 1;
+
+		//выключаем включаем шину
+		I2C1 ->CR1 &= (~I2C_CR1_PE);
+		HAL_Delay(100);
+		I2C1 ->CR1 |= I2C_CR1_PE;
+
+		I2C1 ->CR1 |= I2C_CR1_SWRST;//сброс логики после каждого рестарта
+
+		_lastBtnState = BTN_RELEASE;
+}
+
+
+#define DELAY_RECEIVE_END	400	//(10 * 50/*sec*/) //задержка вызова. при значении в скобках 10 = 1 секунде
+void __attribute__((optimize("O0"))) I2CReceive()
+{
+	HAL_I2C_StateTypeDef resGetState = HAL_I2C_GetState(_hi2c1);
+	static uint32_t startLocalCounter = 0;
+
+	switch (_usrI2CData.PhaseReceive)
+	{
+		case RECEIVE_START:
+			HAL_I2C_Slave_Receive_IT(_hi2c1, _usrI2CData.aRxBuffer, _usrI2CData.sizeRxCmd);
+			_usrI2CData.PhaseReceive = RECEIVE_WAIT_DATA;
+			startLocalCounter = GetSysCounter100MSec();
+			break;
+		case RECEIVE_WAIT_DATA:
+			//если вышло время выделенное на прием
+			if((GetSysCounter100MSec() - startLocalCounter) > DELAY_RECEIVE_END)
+				_usrI2CData.PhaseReceive = RECEIVE_TIMOUT;
+
+			break;
+		default:
+			break;
+	}
+
+}
+
+void __attribute__((optimize("O0"))) I2CSend()
+{
+	HAL_I2C_StateTypeDef resGetState = HAL_I2C_GetState(_hi2c1);
+	static uint32_t startLocalCounter = 0;
+
+	switch (_usrI2CData.PhaseSend)
+	{
+		case SEND_START_NOW:
+//			if(resGetState == HAL_I2C_STATE_READY)
+//			{
+				HAL_I2C_Master_Transmit_IT(_hi2c1, 102, (uint8_t*)(_usrI2CData.aTxBuffer), _usrI2CData.sizeTxCmd);
+				_usrI2CData.PhaseSend = SEND_WAS_START;
+				startLocalCounter = GetSysCounter100MSec();
+//			}
+			break;
+
+		case SEND_WAS_START:
+			if((GetSysCounter100MSec() - startLocalCounter) > DELAY_RECEIVE_END)
+				_usrI2CData.PhaseSend = SEND_TIMOUT;
+			break;
+
+		default:
+			break;
+	}
+
+}
+
+
+void __attribute__((optimize("O0"))) PrepData()
+{
+
+
+	//старт после нажатия кнопки
+	_btnState = HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3);
+	if(_btnState == BTN_RELEASE && _lastBtnState == BTN_PUSH &&
+			_usrI2CData.PhaseSend == SEND_NEUTRAL && _usrI2CData.PhaseReceive == RECEIVE_NEUTRAL)
+	{
+		_usrI2CData.PhaseSend = SEND_START_NOW;
+		_usrI2CData.sizeTxCmd = ST1_SIZE_REQUEST;
+	}
+	_lastBtnState = _btnState;
+
+
+
+//	if(_usrI2CData.PhaseSend == SEND_WAS_START)
+//	{
+//
+//		_usrI2CData.PhaseSend == SEND_WAS_GOOD_END;
+//	}
+
+	if(_usrI2CData.PhaseSend == SEND_WAS_GOOD_END)
+	{
+		_usrI2CData.PhaseSend = SEND_NEUTRAL;
+		_usrI2CData.PhaseReceive = RECEIVE_START;
+		_usrI2CData.sizeRxCmd = P1S2_SIZE_ANSW;
+	}
+
+	//начнем сначала
+	if(_usrI2CData.PhaseReceive == RECEIVE_TIMOUT)
+		_usrI2CData.PhaseReceive = RECEIVE_NEUTRAL;
+
+	if(_usrI2CData.PhaseSend == SEND_TIMOUT)
+		_usrI2CData.PhaseSend = SEND_NEUTRAL;
+
+
+	//какието данные приняты И код = "раздача адресов фаза 1 сбор ID"
+	if(_usrI2CData.PhaseReceive == RECEIVE_YES_ANY_DATA &&
+			_usrI2CData.aRxBuffer[0] == I2CCODE_GET_ID_REQUEST)
+	{
+		_usrI2CData.PhaseReceive = RECEIVE_NEUTRAL;
+		_usrI2CData.PhaseSetAddr = ST1__ID_GRANTED;
+	}
+//
+//	switch (_usrI2CData.PhaseSetAddr)
+//	{
+//		case PH1_GET_ID__SEND_ANSW:
+//			_usrI2CData.aTxBuffer[0] = I2CCODE_GET_ID_REQUEST;//код в ответной команде слейва тот же
+////				_usrI2CData.aTxBuffer[1] =  nI2C;//ID слейва
+//			memset(_usrI2CData.aRxBuffer, 0, SZ_ARR_RX_BUFF);
+//			_usrI2CData.PhaseSend = SEND_START_NOW;
+//			_usrI2CData.PhaseSetAddr = PH1_GET_ID__SEND_ANSW_MADE;
+//			break;
+//
+//		default:
+//			break;
+//	}
+
+
+}
+
+
+
+void  __attribute__((optimize("O0"))) HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	_usrI2CData.PhaseSend = SEND_WAS_GOOD_END;
+}
+void  __attribute__((optimize("O0"))) HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	asm("nop");
+}
+void  __attribute__((optimize("O0"))) HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	asm("nop");
+}
+void  __attribute__((optimize("O0"))) HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+//	SetPhaseReceive(0, RECEIVE_YES_ANY_DATA);
+	_usrI2CData.PhaseReceive = RECEIVE_YES_ANY_DATA;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "I2CSlave.h"
 #include <string.h>
 
