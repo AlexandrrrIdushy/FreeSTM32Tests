823daf8dbe9ce8bd01fbde58200b3a45085d9381
GravitAU <idushy@ya.ru>
2020-11-13 10:21:48 +0400

    Часть 2. компилится с кучей варнингов

--- SRC/LAN/01_narodstreamru_L91_LAN_W5500_STM32F407VG_AtollicTS/Proj/Src/w5500.c
+++ SRC/LAN/01_narodstreamru_L91_LAN_W5500_STM32F407VG_AtollicTS/Proj/Src/w5500.c
@@ -22,6 +22,47 @@ extern uint8_t ipmask[4];
 
 extern uint16_t local_port;
 
+
+//-----------------------------------------------
+//функция записи байта в регистр
+void w5500_writeReg(uint8_t op, uint16_t addres, uint8_t data)
+
+{
+
+  uint8_t buf[] = {addres >> 8, addres, op|(RWB_WRITE<<2), data};
+
+  SS_SELECT();
+
+  HAL_SPI_Transmit(&hspi1, buf, 4, 0xFFFFFFFF);
+
+  SS_DESELECT();
+
+}
+
+
+//-----------------------------------------------
+
+void w5500_ini(void)
+
+{
+
+  uint8_t dtt=0;
+
+  uint8_t opcode=0;
+
+  //Hard Reset
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+
+  HAL_Delay(70);
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+
+  HAL_Delay(70);
+
+}
+
+
 //-----------------------------------------------
 
 static void Error (void)
