1f9ec06c72a8ef9edfee9b758c1a395fa2303acf
GravitAU <idushy@ya.ru>
2020-11-30 11:02:22 +0400

    __ заменить все файлы целиком на скачаные приложения к уроку enc28j60.с компилится

--- SRC/LAN/07_narodstreamru_L68_LAN_ENC28J60_STM32F407VG_AtlTS_CB610/Proj/Core/Src/enc28j60.c
+++ SRC/LAN/07_narodstreamru_L68_LAN_ENC28J60_STM32F407VG_AtlTS_CB610/Proj/Core/Src/enc28j60.c
@@ -1,210 +1,130 @@
 #include "enc28j60.h"
-//--------------------------------------------------
-//Подключим шины SPI и USART в файле enc28j60.c
-extern UART_HandleTypeDef huart1;
+//-----------------------------------------------
 extern SPI_HandleTypeDef hspi1;
-
-
-
-//Создадим переменную для хранения номера текущего банка
-
-uint8_t Enc28j60Bank;
+extern UART_HandleTypeDef huart1;
+//-----------------------------------------------
+static uint8_t Enc28j60Bank;
 static int gNextPacketPtr;
-//L4. Добавим в наш файл глобальную переменную нашего физического адреса
 uint8_t macaddr[6]=MAC_ADDR;
-
-//--------------------------------------------------
-void Error (void)
+//-----------------------------------------------
+static void Error (void)
 {
- LD_ON;
+	LD_ON;
 }
-
-
-//--------------------------------------------------
-//После функции обработки ошибок создадим функии для работы с шиной SPI (подобными функциями мы пользовались при работе с акселерометром на плате Discovery 4 здесь)
+//-----------------------------------------------
 uint8_t SPIx_WriteRead(uint8_t Byte)
 {
-  uint8_t receivedbyte = 0;
-  if(HAL_SPI_TransmitReceive(&hspi1, (uint8_t*) &Byte, (uint8_t*) &receivedbyte, 1, 0x1000) != HAL_OK)
-  {
-    Error();
-  }
-  return receivedbyte;
+	uint8_t receivedbyte = 0;
+	if(HAL_SPI_TransmitReceive(&hspi1,(uint8_t*) &Byte,(uint8_t*) &receivedbyte,1,0x1000)!=HAL_OK)
+	{
+		Error();
+	}
+	return receivedbyte;
 }
-//--------------------------------------------------
+//-----------------------------------------------
 void SPI_SendByte(uint8_t bt)
 {
- SPIx_WriteRead(bt);
+	SPIx_WriteRead(bt);
 }
-//--------------------------------------------------
+//-----------------------------------------------
 uint8_t SPI_ReceiveByte(void)
 {
- uint8_t bt = SPIx_WriteRead(0xFF);
- return bt; //вернём регистр данных
+	uint8_t bt = SPIx_WriteRead(0xFF);
+	return bt;
 }
-//--------------------------------------------------
-
-//--------------------------------------------------
-//напишем функцию операции записи байта в регистр
-void enc28j60_writeOp(uint8_t op,uint8_t addres, uint8_t data)
+//-----------------------------------------------
+void enc28j60_writeOp(uint8_t op, uint8_t addres, uint8_t data)
 {
- SS_SELECT();
- SPI_SendByte(op|(addres&ADDR_MASK));
- SPI_SendByte(data);
- SS_DESELECT();
+	SS_SELECT();
+	SPI_SendByte(op|(addres&ADDR_MASK));
+	SPI_SendByte(data);
+	SS_DESELECT();
 }
-//--------------------------------------------------
-//Ну, а теперь, операция чтения
-//--------------------------------------------------
-uint8_t enc28j60_readOp(uint8_t op,uint8_t addres)
+//-----------------------------------------------
+static uint8_t enc28j60_readOp(uint8_t op, uint8_t addres)
 {
- uint8_t result;
- SS_SELECT();
- SPI_SendByte(op|(addres&ADDR_MASK));
- SPI_SendByte(0x00);
- //пропускаем ложный байт
-  if(addres & 0x80) SPI_ReceiveByte();
- result=SPI_ReceiveByte();
-  SS_DESELECT();
-  return result;
+	uint8_t result;
+	SS_SELECT();
+	SPI_SendByte(op|(addres&ADDR_MASK));
+	SPI_SendByte(0x00);
+	//пропускаем ложный байт
+	if(addres & 0x80) SPI_ReceiveByte();
+	result = SPI_ReceiveByte();
+	SS_DESELECT();
+	return result;
 }
-//--------------------------------------------------
-
-
-
-
-//--------------------------------------------------
-//При операции чтения мы читаем регистр ESTAT на предмет установки бита CLKRDY.
-//--------------------------------------------------
-void enc28j60_readBuf(uint16_t len,uint8_t* data)
+//-----------------------------------------------
+static void enc28j60_readBuf(uint16_t len, uint8_t* data)
 {
 	SS_SELECT();
 	SPI_SendByte(ENC28J60_READ_BUF_MEM);
 	while(len--){
-	*data++=SPIx_WriteRead(0x00);
+		*data++=SPIx_WriteRead(0x00);
 	}
 	SS_DESELECT();
 }
-//--------------------------------------------------
-
-
-//А теперь напишем функцию установки текущего банка
-//--------------------------------------------------
+//-----------------------------------------------
+static void enc28j60_writeBuf(uint16_t len, uint8_t* data)
+{
+	SS_SELECT();
+	SPI_SendByte(ENC28J60_WRITE_BUF_MEM);
+	while(len--)
+		SPI_SendByte(*data++);
+	SS_DESELECT();
+}
+//-----------------------------------------------
 void enc28j60_SetBank(uint8_t addres)
 {
-	if ((addres&BANK_MASK)!=Enc28j60Bank)
+	if((addres&BANK_MASK)!=Enc28j60Bank)
 	{
 		enc28j60_writeOp(ENC28J60_BIT_FIELD_CLR,ECON1,ECON1_BSEL1|ECON1_BSEL0);
 		Enc28j60Bank = addres&BANK_MASK;
 		enc28j60_writeOp(ENC28J60_BIT_FIELD_SET,ECON1,Enc28j60Bank>>5);
 	}
 }
-//--------------------------------------------------
-
-//L3. Затем напишем универсальные функции для чтения и записи обычных регистров управления
-
-//--------------------------------------------------
-void enc28j60_writeRegByte(uint8_t addres,uint8_t data)
+//-----------------------------------------------
+void enc28j60_writeRegByte(uint8_t addres, uint8_t data)
 {
 	enc28j60_SetBank(addres);
 	enc28j60_writeOp(ENC28J60_WRITE_CTRL_REG,addres,data);
 }
-//--------------------------------------------------
-uint8_t enc28j60_readRegByte(uint8_t addres)
+//-----------------------------------------------
+static uint8_t enc28j60_readRegByte(uint8_t addres)
 {
 	enc28j60_SetBank(addres);
 	return enc28j60_readOp(ENC28J60_READ_CTRL_REG,addres);
 }
-//--------------------------------------------------
-//L3. Так как все указатели у нас двухбайтовые, то создадим ещё функцию для записи данных в такие регистры
-//--------------------------------------------------
-void enc28j60_writeReg(uint8_t addres,uint16_t data)
+//-----------------------------------------------
+void enc28j60_writeReg(uint8_t addres, uint16_t data)
 {
 	enc28j60_writeRegByte(addres, data);
 	enc28j60_writeRegByte(addres+1, data>>8);
 }
-//--------------------------------------------------
-
-//L4. Поэтому напишем отдельную функцию для записи регистра PHY
-
-
-
-//--------------------------------------------------
-void enc28j60_writePhy(uint8_t addres,uint16_t data)
+//-----------------------------------------------
+static void enc28j60_writePhy(uint8_t addres, uint16_t data)
 {
 	enc28j60_writeRegByte(MIREGADR, addres);
-	enc28j60_writeReg(MIWR, data);
-	while(enc28j60_readRegByte(MISTAT)&MISTAT_BUSY);
+	enc28j60_writeRegByte(MIWR, data);
+	while(enc28j60_readRegByte(MISTAT)&MISTAT_BUSY)
+	;
 }
-
-//--------------------------------------------------
-
-//--------------------------------------------------
-//L4.Создадим функцию для приёма пакета в файле enc28j60.c и создадим в ней локальную переменную для подсчёта длины пакета и последующего его возврата
-uint16_t __attribute__((optimize("O0"))) enc28j60_packetReceive(uint8_t *buf,uint16_t buflen)
-
-{
-	uint16_t len=0;
-//В регистре EPKTCNT хранится количество принятых на данный момент пакетов, поэтому проверим его
-	if(enc28j60_readRegByte(EPKTCNT)>0)
-	{
-		enc28j60_writeReg(ERDPT,gNextPacketPtr);//В регистр ERDPT установим указатель
-		//Начнём считывать пакет. Объявим локльную структуру для заголовка
-		//считаем заголовок
-		struct{
-		  uint16_t nextPacket;
-		  uint16_t byteCount;
-		  uint16_t status;
-		} header;
-		enc28j60_readBuf(sizeof header,(uint8_t*)&header);
-		//Запишем значение указателя на следующий пакет в глобальную переменную
-		gNextPacketPtr=header.nextPacket;
-		//Инициализируем переменную длины пакеты, отрезав от неё контрольную сумму
-		len=header.byteCount-4;//remove the CRC count
-		//Укоротим длину до заданной во входном параметре
-		if(len>buflen) len=buflen;
-		//Проверим статус и считаем буфер
-		if((header.status&0x80)==0)
-			len=0;
-		else enc28j60_readBuf(len, buf);
-		//		Завершим буфер нулём
-		buf[len]=0;
-		//Инициализируем указатель буфера на адрес следующего пакета
-		if(gNextPacketPtr-1>RXSTOP_INIT)
-			enc28j60_writeReg(ERXRDPT,RXSTOP_INIT);
-		else
-			enc28j60_writeReg(ERXRDPT,gNextPacketPtr-1);
-		enc28j60_writeOp(ENC28J60_BIT_FIELD_SET,ECON2,ECON2_PKTDEC);
-
-	}
-	return len;
-}
-
-//--------------------------------------------------
-
-
-//--------------------------------------------------
+//-----------------------------------------------
 void enc28j60_ini(void)
 {
 	LD_OFF;
-	//Произведём "мягкий" сброс в функции инициализации и проверим, что всё перезагрузилось
-	enc28j60_writeOp(ENC28J60_SOFT_RESET,0,ENC28J60_SOFT_RESET);
+	enc28j60_writeOp(ENC28J60_SOFT_RESET,ENC28J60_SOFT_RESET,0);
 	HAL_Delay(2);
 	//проверим, что всё перезагрузилось
-	while(!enc28j60_readOp(ENC28J60_READ_CTRL_REG,ESTAT)&ESTAT_CLKRDY);
-	//L3. Настроим буферы в функции инициализации
+	while(!enc28j60_readOp(ENC28J60_READ_CTRL_REG,ESTAT)&ESTAT_CLKRDY)
+		;
 	//настроим буферы
 	enc28j60_writeReg(ERXST,RXSTART_INIT);
 	enc28j60_writeReg(ERXRDPT,RXSTART_INIT);
 	enc28j60_writeReg(ERXND,RXSTOP_INIT);
 	enc28j60_writeReg(ETXST,TXSTART_INIT);
 	enc28j60_writeReg(ETXND,TXSTOP_INIT);
-
-	//L4. Включим broadcast, вдруг пригодится
 	//Enable Broadcast
 	enc28j60_writeRegByte(ERXFCON,enc28j60_readRegByte(ERXFCON)|ERXFCON_BCEN);
-
-	//L4. настроим канальный уровень в инициализации
 	//настраиваем канальный уровень
 	enc28j60_writeRegByte(MACON1,MACON1_MARXEN|MACON1_TXPAUS|MACON1_RXPAUS);
 	enc28j60_writeRegByte(MACON2,0x00);
@@ -218,22 +138,58 @@ void enc28j60_ini(void)
 	enc28j60_writeRegByte(MAADR2,macaddr[3]);
 	enc28j60_writeRegByte(MAADR1,macaddr[4]);
 	enc28j60_writeRegByte(MAADR0,macaddr[5]);
-
-	//L4. Теперь настроим физический уровень в инициализации
-
-	enc28j60_writeRegByte(MAADR0,macaddr[5]);
 	//настраиваем физический уровень
 	enc28j60_writePhy(PHCON2,PHCON2_HDLDIS);//отключаем loopback
 	enc28j60_writePhy(PHLCON,PHLCON_LACFG2| //светодиоды
-	 PHLCON_LBCFG2|PHLCON_LBCFG1|PHLCON_LBCFG0|
-	 PHLCON_LFRQ0|PHLCON_STRCH);
-
-	//L4. Ну и окончательные настройки в инициализации
-
-
+		PHLCON_LBCFG2|PHLCON_LBCFG1|PHLCON_LBCFG0|
+		PHLCON_LFRQ0|PHLCON_STRCH);
 	enc28j60_SetBank(ECON1);
 	enc28j60_writeOp(ENC28J60_BIT_FIELD_SET,EIE,EIE_INTIE|EIE_PKTIE);
 	enc28j60_writeOp(ENC28J60_BIT_FIELD_SET,ECON1,ECON1_RXEN);//разрешаем приём пакетов
-	//L4. Вот и вся инициализация. Наконец то!
 }
-
+//-----------------------------------------------
+uint16_t enc28j60_packetReceive(uint8_t* buf, uint16_t buflen)
+{
+	uint16_t len=0;
+	if(enc28j60_readRegByte(EPKTCNT)>0)
+	{
+		enc28j60_writeReg(ERDPT,gNextPacketPtr);
+		//считаем заголовок
+		struct{
+			uint16_t nextPacket;
+			uint16_t byteCount;
+			uint16_t status;
+		} header;
+		enc28j60_readBuf(sizeof(header), (uint8_t*)&header);
+		gNextPacketPtr=header.nextPacket;
+		len = header.byteCount-4;//remove the CRC count
+		if(len>buflen) len=buflen;
+		if((header.status&0x80)==0) len=0;
+		else enc28j60_readBuf(len, buf);
+		buf[len]=0;
+		if(gNextPacketPtr-1>RXSTOP_INIT)
+			enc28j60_writeReg(ERXRDPT, RXSTOP_INIT);
+		else
+			enc28j60_writeReg(ERXRDPT, gNextPacketPtr-1);
+		enc28j60_writeOp(ENC28J60_BIT_FIELD_SET,ECON2,ECON2_PKTDEC);
+	}
+	return len;
+}
+//-----------------------------------------------
+void enc28j60_packetSend(uint8_t* buf, uint16_t buflen)
+{
+	while(!enc28j60_readOp(ENC28J60_READ_CTRL_REG,ECON1)&ECON1_TXRTS)
+	{
+		if(enc28j60_readRegByte(EIR)&EIR_TXERIF)
+		{
+			enc28j60_writeOp(ENC28J60_BIT_FIELD_SET, ECON1,ECON1_TXRST);
+			enc28j60_writeOp(ENC28J60_BIT_FIELD_CLR, ECON1,ECON1_TXRST);
+		}
+	}
+	enc28j60_writeReg(EWRPT, TXSTART_INIT);
+	enc28j60_writeReg(ETXND, TXSTART_INIT+buflen);
+	enc28j60_writeBuf(1,(uint8_t*)"\x00");
+	enc28j60_writeBuf(buflen,buf);
+	enc28j60_writeOp(ENC28J60_BIT_FIELD_SET, ECON1,ECON1_TXRTS);
+}
+//-----------------------------------------------
