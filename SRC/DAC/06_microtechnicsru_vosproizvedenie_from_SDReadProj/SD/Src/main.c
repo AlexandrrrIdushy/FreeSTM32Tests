60efd6d03ca8250a9aad78c9df8bec56203de0f8
GravitAU <idushy@ya.ru>
2020-12-14 15:31:32 +0400

    сравнил с скачанным майном - привел к такому виду

--- SRC/DAC/06_microtechnicsru_vosproizvedenie_from_SDReadProj/SD/Src/main.c
+++ SRC/DAC/06_microtechnicsru_vosproizvedenie_from_SDReadProj/SD/Src/main.c
@@ -34,7 +34,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define	DEBUG_TEST_READ_TEXT_FROM_SD
+//#define	DEBUG_TEST_READ_TEXT_FROM_SD
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -78,11 +78,11 @@ static void MX_TIM6_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#ifdef	DEBUG_TEST_READ_TEXT_FROM_SD
 FATFS SDFatFs;  /* File system object for SD card logical drive */
-
 FIL MyFile;     /* File object */
-
 char SD_Path[4];  /* SD logical drive path */
+#endif
 /* USER CODE END 0 */
 
 /**
@@ -118,48 +118,43 @@ int main(void)
   MX_FATFS_Init();
   MX_DAC1_Init();
   MX_TIM6_Init();
-  /* USER CODE BEGIN 2 */
-//  BSP_SD_Init();
-//  HAL_Delay(200);
-//  MX_FATFS_Init();
 
   /* USER CODE BEGIN 2 */
-//  res = f_mount(&fileSystem, SDPath, 1);
-//  uint8_t path[10] = "audio.wav";
-//  res = f_open(&audioFile, (char*)path, FA_READ);
-//
-//  /* USER CODE END 2 */
-//
-//  /* Infinite loop */
-//  /* USER CODE BEGIN WHILE */
-//  //#1
-//  uint8_t* bw;
-//  FRESULT res;
-//  //#2
-//  //Таким образом, находим позицию в буфере, которая соответствует символу ‘d’ и прибавляем к этому значению 8 (4 байта для ‘data’ и 4 байта для размера данных):
-//  uint16_t dataOffset = 0;
-//  res = f_read(&audioFile, wavBuf[0], WAV_BUF_SIZE, &readBytes);
-//  for (uint16_t i = 0; i < (WAV_BUF_SIZE - 3); i++)
-//  {
-//      if ((wavBuf[0][i] == 'd') && (wavBuf[0][i + 1] == 'a') &&
-//          (wavBuf[0][i + 2] == 't') && (wavBuf[0][i + 3] == 'a'))
-//      {
-//          dataOffset = i + 8;
-//          break;
-//      }
-//  }
-//  //#3
-//  //Заголовок обнаружен, перемещаем указатель FatFs для работы с файлом на аудио-данные и заодно определяем количество байт данных. Для этого вычитаем из общего размера файла размер заголовка:
-//  res = f_lseek(&audioFile, dataOffset);
-//  wavDataSize = f_size(&audioFile) - dataOffset;
-//  //#3
-////  Реализуем этот механизм и, первым делом, заполняем оба буфера данными:
-//    res = f_read(&audioFile, wavBuf[0], WAV_BUF_SIZE, &readBytes);
-//    res = f_read(&audioFile, wavBuf[1], WAV_BUF_SIZE, &readBytes);
-//    //#5
-////  Поскольку данные готовы, спокойно включаем DAC и TIM6 на генерацию прерываний:
-//    HAL_DAC_Start(&hdac, DAC_CHANNEL_2);
-//    HAL_TIM_Base_Start_IT(&htim6);
+  res = f_mount(&fileSystem, SDPath, 1);
+  uint8_t path[10] = "audio.wav";
+  res = f_open(&audioFile, (char*)path, FA_READ);
+  //#2
+  //Таким образом, находим позицию в буфере, которая соответствует символу ‘d’ и прибавляем к этому значению 8 (4 байта для ‘data’ и 4 байта для размера данных):
+  uint16_t dataOffset = 0;
+  res = f_read(&audioFile, wavBuf[0], WAV_BUF_SIZE, &readBytes);
+  for (uint16_t i = 0; i < (WAV_BUF_SIZE - 3); i++)
+  {
+      if ((wavBuf[0][i] == 'd') && (wavBuf[0][i + 1] == 'a') &&
+          (wavBuf[0][i + 2] == 't') && (wavBuf[0][i + 3] == 'a'))
+      {
+          dataOffset = i + 8;
+          break;
+      }
+  }
+
+  //#3
+  //Заголовок обнаружен, перемещаем указатель FatFs для работы с файлом на аудио-данные и заодно определяем количество байт данных. Для этого вычитаем из общего размера файла размер заголовка:
+  res = f_lseek(&audioFile, dataOffset);
+  wavDataSize = f_size(&audioFile) - dataOffset;
+  //#3
+//  Реализуем этот механизм и, первым делом, заполняем оба буфера данными:
+    res = f_read(&audioFile, wavBuf[0], WAV_BUF_SIZE, &readBytes);
+    res = f_read(&audioFile, wavBuf[1], WAV_BUF_SIZE, &readBytes);
+    //#5
+//  Поскольку данные готовы, спокойно включаем DAC и TIM6 на генерацию прерываний:
+    HAL_DAC_Start(&hdac, DAC_CHANNEL_2);
+    HAL_TIM_Base_Start_IT(&htim6);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+
 
 #ifdef	DEBUG_TEST_READ_TEXT_FROM_SD
   //код проверки чтения SD карты
@@ -207,32 +202,34 @@ int main(void)
 	      res = f_close(&audioFile);
 	      stopFlag = 0;
 	  }
-	  //#
-//	  В общем-то по окончанию файла отключаем таймер и выставляем флаг stopFlag в 1:
 
-	  if (curWavIdx >= wavDataSize)
-	  {
-	      HAL_TIM_Base_Stop_IT(&htim6);
-	      stopFlag = 1;
-	  }
 
-	  //#
-	  //Если конец файла еще не достигнут, то проверяем счетчик curBufOffset. Если значение равно 512 (WAV_BUF_SIZE), то буфер подошел к концу, а значит надо изменить номер активного буфера и выставить флаг wavReadFlag:
-	  if (curBufOffset == WAV_BUF_SIZE)
-	  {
-	      curBufOffset = 0;
-
-	      if (curBufIdx == 0)
-	      {
-	          curBufIdx = 1;
-	      }
-	      else
-	      {
-	          curBufIdx = 0;
-	      }
-
-	      wavReadFlag = 1;
-	  }
+//	  //# ? дальшейшего кода нет в скачанном исходнике
+////	  В общем-то по окончанию файла отключаем таймер и выставляем флаг stopFlag в 1:
+//
+//	  if (curWavIdx >= wavDataSize)
+//	  {
+//	      HAL_TIM_Base_Stop_IT(&htim6);
+//	      stopFlag = 1;
+//	  }
+//
+//	  //#
+//	  //Если конец файла еще не достигнут, то проверяем счетчик curBufOffset. Если значение равно 512 (WAV_BUF_SIZE), то буфер подошел к концу, а значит надо изменить номер активного буфера и выставить флаг wavReadFlag:
+//	  if (curBufOffset == WAV_BUF_SIZE)
+//	  {
+//	      curBufOffset = 0;
+//
+//	      if (curBufIdx == 0)
+//	      {
+//	          curBufIdx = 1;
+//	      }
+//	      else
+//	      {
+//	          curBufIdx = 0;
+//	      }
+//
+//	      wavReadFlag = 1;
+//	  }
 
 
     /* USER CODE END WHILE */
@@ -427,45 +424,45 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-//void TIM6_IRQHandler(void)
-//{
-//    /* USER CODE BEGIN TIM6_IRQn 0 */
-//
-//    /* USER CODE END TIM6_IRQn 0 */
-//    HAL_TIM_IRQHandler(&htim6);
-//    /* USER CODE BEGIN TIM6_IRQn 1 */
-//    uint16_t dacData = (((wavBuf[curBufIdx][curBufOffset + 1] << 8) | wavBuf[curBufIdx][curBufOffset]) + 32767);
-//    dacData /= 16;
-//    HAL_DAC_SetValue(&hdac, DAC_CHANNEL_2, DAC_ALIGN_12B_R, dacData);
-//
-//    curBufOffset += 2;
-//    curWavIdx += 2;
-//
-//    if (curWavIdx >= wavDataSize)
-//    {
-//        HAL_TIM_Base_Stop_IT(&htim6);
-//        stopFlag = 1;
-//    }
-//    else
-//    {
-//        if (curBufOffset == WAV_BUF_SIZE)
-//        {
-//            curBufOffset = 0;
-//
-//            if (curBufIdx == 0)
-//            {
-//                curBufIdx = 1;
-//            }
-//            else
-//            {
-//                curBufIdx = 0;
-//            }
-//
-//            wavReadFlag = 1;
-//        }
-//    }
-//    /* USER CODE END TIM6_IRQn 1 */
-//}
+void TIM6_IRQHandler(void)
+{
+    /* USER CODE BEGIN TIM6_IRQn 0 */
+
+    /* USER CODE END TIM6_IRQn 0 */
+    HAL_TIM_IRQHandler(&htim6);
+    /* USER CODE BEGIN TIM6_IRQn 1 */
+    uint16_t dacData = (((wavBuf[curBufIdx][curBufOffset + 1] << 8) | wavBuf[curBufIdx][curBufOffset]) + 32767);
+    dacData /= 16;
+    HAL_DAC_SetValue(&hdac, DAC_CHANNEL_2, DAC_ALIGN_12B_R, dacData);
+
+    curBufOffset += 2;
+    curWavIdx += 2;
+
+    if (curWavIdx >= wavDataSize)
+    {
+        HAL_TIM_Base_Stop_IT(&htim6);
+        stopFlag = 1;
+    }
+    else
+    {
+        if (curBufOffset == WAV_BUF_SIZE)
+        {
+            curBufOffset = 0;
+
+            if (curBufIdx == 0)
+            {
+                curBufIdx = 1;
+            }
+            else
+            {
+                curBufIdx = 0;
+            }
+
+            wavReadFlag = 1;
+        }
+    }
+    /* USER CODE END TIM6_IRQn 1 */
+}
 /* USER CODE END 4 */
 
 /**
