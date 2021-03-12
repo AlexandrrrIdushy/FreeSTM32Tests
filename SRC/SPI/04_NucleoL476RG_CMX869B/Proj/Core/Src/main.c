628c57aaf37d2ba7a3bf22fac4077fa8989b2fd0
GravitAU <idushy@ya.ru>
2021-03-12 14:36:47 +0400

    CUBE. понизил частоту. SPI так и не запустился

--- SRC/SPI/04_NucleoL476RG_CMX869B/Proj/Core/Src/main.c
+++ SRC/SPI/04_NucleoL476RG_CMX869B/Proj/Core/Src/main.c
@@ -95,7 +95,7 @@ int main(void)
   MX_SPI3_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-  aTxBuffer[0] = 0xAA;
+  aTxBuffer[0] = 0xBB;
 
 //  cs_strob();
   /* USER CODE END 2 */
@@ -147,11 +147,11 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
     Error_Handler();
   }
