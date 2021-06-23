3138f0f019210d107d361ac443edf12b055ec959
AlexandrrrIdushy <alexanderiyxx@gmail.com>
2021-06-23 16:18:40 +0400

    С. dac1. прерывания on.

--- SRC/DAC/07_microtechnicsru_vosproizvedenie_from_SDReadProj_a bit steps/Proj/Src/stm32l4xx_it.c
+++ SRC/DAC/07_microtechnicsru_vosproizvedenie_from_SDReadProj_a bit steps/Proj/Src/stm32l4xx_it.c
@@ -56,6 +56,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern DAC_HandleTypeDef hdac1;
 extern TIM_HandleTypeDef htim2;
 /* USER CODE BEGIN EV */
 
@@ -209,6 +210,20 @@ void TIM2_IRQHandler(void)
   /* USER CODE BEGIN TIM2_IRQn 1 */
 
   /* USER CODE END TIM2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM6 global interrupt, DAC channel1 and channel2 underrun error interrupts.
+  */
+void TIM6_DAC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+  /* USER CODE END TIM6_DAC_IRQn 0 */
+  HAL_DAC_IRQHandler(&hdac1);
+  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+
+  /* USER CODE END TIM6_DAC_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
