9f7aab82ac989dbe37b86d1c64b05ae3cfec01ad
xxx <xxx@ya.ru>
2021-12-28 15:14:26 +0400

    похоже проблема в HAL_Delay()

--- SRC/Bootloader/09_SimpleBootloader_L476RG/App/Core/Src/main.c
+++ SRC/Bootloader/09_SimpleBootloader_L476RG/App/Core/Src/main.c
@@ -96,7 +96,10 @@ int main(void)
 	  asm("nop");
 	  t++;
 //	  HAL_Delay(500);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_SET);
+	  HAL_Delay(500);
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
+//	  HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
